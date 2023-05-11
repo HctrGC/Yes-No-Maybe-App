996ffcf95a887de6b11276bd1fff0519d934ecd1
HctrGC <batshurk@gmail.com>
2023-05-11 12:18:31 -0600

    yes-no-maybe app finished

---
+++ lib/domain/entities/message.dart
@@ -0,0 +1,14 @@
+
+enum FromWho { me, theirs }
+
+class Message {
+  final String text;
+  final String? imageUrl;
+  final FromWho fromWho;
+
+  Message({
+    required this.text,
+    required this.fromWho,
+    this.imageUrl
+  });
+}
